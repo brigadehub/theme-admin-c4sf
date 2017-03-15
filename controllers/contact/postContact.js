4d25f3050fb365868b2f692ce5c9723fe899ef8b
Perry Taylor <pltaylor3@gmail.com>
2017-03-15 12:06:23 -0400

    ✨  Add POST Contacts (#17)
    * Try to prevent null errors from bringing down the app.
    
    * fix post action to be /admi/
    
    * More explicit error handling from feedback.
    
    * Initial mocking out of postContact.js
    
    * get contact to pull up and update rank.
    
    * Try to change the form control to ensure it gets passed, but no go.
    
    * Set up project.json so linting from make works.
    
    * Fix display check box so it updates the record.
    
    * refactor contact names to 'User'
    
    * move success message in code to accurately reflect success.
    
    * handle errors better in the post contact controller.

---
+++ controllers/contact/postContact.js
@@ -0,0 +1,39 @@
+module.exports = {
+  method: 'post',
+  endpoint: '/contact',
+  authenticated: true,
+  roles: ['core', 'superAdmin'],
+  scopes: ['user:email', 'repo', 'admin:org', 'admin:repo_hook', 'admin:org_hook'],
+  middleware: [],
+  controller: postContactsManage
+}
+
+function postContactsManage (req, res) {
+  var Users = req.models.Users
+
+  var mongooseQuery = {}
+
+  Users.find(mongooseQuery, function (err, users) {
+    if (err) console.error(err)
+    users.forEach(function (user) {
+      var userInfo = req.body[user.username]
+      if (!userInfo.showcontact) {
+        user.profile.showcontact = false
+      } else {
+        user.profile.showcontact = true
+      }
+      user.profile.contactpagerank = userInfo.contactrank
+      user.save(function (err) {
+        if (err) {
+          // save failed
+          req.flash('errors', {msg: 'Error! Your Contacts were not updated. Error: ' + err})
+          console.log('User update failed.  Error:' + err)
+          return res.redirect('contact/edit')
+        }
+        // save is successfull.
+        req.flash('success', {msg: 'Success! You updated contacts.'})
+        return res.redirect('contact/edit')
+      })
+    })
+  })
+}
